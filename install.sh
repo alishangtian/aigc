05198d1b616841b17c83aeadf7e0d0b658cbee33
alishangtian <alishangtian@gmail.com>
2024-11-28 19:13:27 +0800

    cache_dir=cache_dir

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+export HF_HOME=/data2/.cache
 export HF_TOKEN=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
 export HF_ENDPOINT=https://hf-mirror.com
 pip install -r requirments.txt -i https://mirrors.cloud.tencent.com/pypi/simple
